dd8107a319220dbc54aabf0f8549fca7eb9f08ab
Wistfy <huy.vd76@gmail.com>
2026-01-10 23:20:26 +0700

    Fixing issue: - Accessibility, Logic error, , weak password validation, Method visibility, Bitwise Operation Error, Uninitialized dependency Implemented Password hasher use PBKDF2

--- FinanceManager.Domain/Entities/WalletDetail.cs
+++ FinanceManager.Domain/Entities/WalletDetail.cs
@@ -1,23 +1,34 @@
-﻿using System;
+﻿using FinanceManager.Domain.Exception;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FinanceManager.Domain.Entities
 {
-    internal class WalletDetail
+    public class WalletDetail
     {
-        private int _walletDetailID { get; }
+        public int _walletDetailID {get; }
         private int _walletID { get;}
         private string _name { get; set; }
         private decimal _fund { get; set; }
         private decimal _current { get; set; }
         private DateTime _updatedDate { get; set; }
 
-        public WalletDetail(string name, decimal fund, decimal current) { 
+        public WalletDetail(string name, decimal fund, decimal current) {
+            if (fund < 0) throw new DomainException("Value is not valid");   
+
             this._name = name;
             this._fund = fund;
             this._current = current;
             this._updatedDate = DateTime.Now;
         }
+
+        public void Update(WalletDetail walletDetail) {
+            _name = walletDetail._name;
+            _fund = walletDetail._fund;
+            _current = walletDetail._current;
+            _updatedDate = walletDetail._updatedDate;
+        }
+        
     }
 }
